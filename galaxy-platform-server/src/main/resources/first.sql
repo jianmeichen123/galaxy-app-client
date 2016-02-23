f5575e1686227ae5cf03f5c7d9aa7a161008ad0c
xingkaihu <kaihuxing@galaxyinternet.com>
2016-02-23 17:38:56 +0800

    添加一测试脚本

---
+++ galaxy-platform-server/src/main/resources/first.sql
@@ -0,0 +1,44 @@
+/*初始化测试脚本*/
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.6.24 : Database - galaxy_star
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`galaxy_star` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `galaxy_star`;
+
+/*Table structure for table `t_user` */
+
+DROP TABLE IF EXISTS `t_user`;
+
+CREATE TABLE `t_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `real_name` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `mobile` varchar(12) DEFAULT NULL,
+  `telephone` varchar(20) DEFAULT NULL,
+  `address` varchar(300) DEFAULT NULL,
+  `created_time` bigint(20) DEFAULT NULL,
+  `updated_time` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_user` */
+
+insert  into `t_user`(`id`,`real_name`,`email`,`birthday`,`mobile`,`telephone`,`address`,`created_time`,`updated_time`) values (1,'zhangsan','zhangsan@163.com','2016-02-17','2147483647','010-676767676','海淀区',111111111,NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
