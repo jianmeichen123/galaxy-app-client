b099bd495a3331107c7b49873daad79f318c972a
Vincent <leizhang@galaxyinternet.com>
2016-02-26 11:39:07 +0800

    数据字典dao

---
+++ galaxy-platform-api/src/main/java/com/galaxyinternet/dao/dict/DictDao.java
@@ -0,0 +1,37 @@
+package com.galaxyinternet.dao.dict;
+
+import java.util.List;
+
+import com.galaxyinternet.framework.core.dao.BaseDao;
+import com.galaxyinternet.model.dict.BatchDictInsetParam;
+import com.galaxyinternet.model.dict.Dict;
+
+/**
+ * 
+    * @ClassName: DictDao
+    * @Description: TODO(这里用一句话描述这个类的作用)
+    * @company 星河互联
+    * @author vincent
+    * @date 2016年2月24日 下午1:25:09 
+    *
+ */
+public interface DictDao extends BaseDao<Dict, Long>{
+
+	
+	int selectCountByParentId(Long parentId);
+	
+	List<Dict> selectByParentId(Long parentId);
+	
+	Dict selectByCode(String code);
+	
+	int selectCountInCodes(List<String> codes);
+	
+	int selectCountSame(Dict dict);
+	
+	int selectCountSameIn(BatchDictInsetParam batchDictInsetParam);
+	
+	Dict selectByParentIdAndName(Dict dict);
+
+	int insertInBatch(BatchDictInsetParam batchDictInsetParam);
+
+}
