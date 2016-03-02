de13c6344bd480c7e5a9d07672f3db10fb115812
xingkaihu <kaihuxing@galaxyinternet.com>
2016-03-02 15:10:47 +0800

    添加localstrage的通用方法

--- galaxy-platform-server/src/main/webapp/js/common.js
+++ galaxy-platform-server/src/main/webapp/js/common.js
@@ -70,3 +70,40 @@ function sendPostRequest(reqUrl, jsonData, sessionId, callbackFun) {
 function sendGetRequest(reqUrl, jsonData, sessionId, callbackFun) {
 	sendAjaxRequest("GET", reqUrl, jsonData, sessionId, callbackFun);
 }
+
+/**
+ * 使用localstage存储数据 <br/>
+ * 注意：IE、Firefox测试的时候需要把文件上传到服务器上（或者localhost），直接点开本地的HTML文件，是不行的。
+ * 
+ * DataStrore.
+ */
+DataStrore = {
+	storage : window.localStorage,
+	checkBrowerSupport : function() {
+		if (window.localStorage) {
+			alert('This browser supports localStorage');
+			return true;
+		} else {
+			alert('This browser does not support localStorage');
+			return false;
+		}
+	},
+	addElement : function(key, value) {
+		dataStrore.stroage.setItem(key, value);
+	},
+	getElement : function(key) {
+		dataStrore.stroage.getItem(key);
+	},
+	removeElement : function(key) {
+		dataStrore.stroage.removeItem(key);
+	},
+	removeAll : function() {
+		dataStrore.stroage.clear();
+	},
+	showKeysAndValues : function() {
+		for (var i = 0; i < dataStrore.storage.length; i++) {
+			document.write(storage.key(i) + " : "
+					+ storage.getItem(storage.key(i)) + "<br>");
+		}
+	}
+}
