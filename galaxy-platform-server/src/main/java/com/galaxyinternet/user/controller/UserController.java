e20f5dbfd7588a5d4fe1bba14d6f18bc63a022e7
zhaoying <yingzhao@galaxyunternet.com>
2016-03-02 11:02:25 +0800

    --zhaoying--控制层修改

--- galaxy-platform-server/src/main/java/com/galaxyinternet/user/controller/UserController.java
+++ galaxy-platform-server/src/main/java/com/galaxyinternet/user/controller/UserController.java
@@ -34,6 +34,7 @@ import com.galaxyinternet.service.UserService;
 
 /**
  * 用户相关
+ * 
  * @author zhaoying
  *
  */
@@ -43,7 +44,7 @@ public class UserController extends BaseControllerImpl<User, UserBo> {
 	final Logger logger = LoggerFactory.getLogger(UserController.class);
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private DepartmentService departmentService;
 	@Override
@@ -51,19 +52,20 @@ public class UserController extends BaseControllerImpl<User, UserBo> {
 		return this.userService;
 	}
 
-	
 	/**
 	 * 默认页面
 	 */
 	@RequestMapping(method = RequestMethod.GET)
-	public String list(HttpServletRequest request,HttpServletResponse response) {
-		//部门列表
+	public String list(HttpServletRequest request,
+			HttpServletResponse response) {
+	/*	// 部门列表
 		List<Department> deptList = departmentService.queryAll();
-		//默认取一页数据
+		// 默认取一页数据
 		PageRequest pageable = new PageRequest();
-		Page<User> page = userService.queryUserList(null, pageable);
-		request.setAttribute("deptList",deptList);
-		request.setAttribute("content",page.getContent());
+		Page<User> page = userService.queryUserList(new User(), pageable);
+		request.setAttribute("deptList", deptList);
+		request.setAttribute("content", page.getContent());
+		*/ 
 		return "system/user/user_list";
 	}
 
@@ -76,66 +78,98 @@ public class UserController extends BaseControllerImpl<User, UserBo> {
 	@ResponseBody
 	@RequestMapping(value = "/resetPwd", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseData<User> resetPwd(@RequestBody User user) {
-		userService.resetPwd(user);
+		
+		ResponseData<User> responseBody = new ResponseData<User>();
+		
+		try {
+			userService.resetPwd(user);
+			responseBody.setResult(new Result(Status.OK, user));
+
+		} catch (PlatformException e) {
+			responseBody
+					.setResult(new Result(Status.ERROR, "resetPwd faild"));
+
+			if (logger.isErrorEnabled()) {
+				logger.error("resetPwd ", e);
+			}
+		}
+		
 		String toMail = user.getEmail(); // "sue_vip@126.com"; 收件人邮件地址
-		String content = "<html>" + "<head></head>" + "<body>" + "<div align=center>"
+		String content = "<html>" + "<head></head>" + "<body>"
+				+ "<div align=center>"
 				+ "	<a href=http://localhost:8000/controller/vcs/login/toLogin target=_blank>"
-				+ "您好，您密码已重置，请点击地址：http://localhost:8000/controller/vcs/login/toLogin  登陆 " + "	</a>" + "</div>"
-				+ "</body>" + "</html>";// 邮件内容
+				+ "您好，您密码已重置，请点击地址：http://localhost:8000/controller/vcs/login/toLogin  登陆 "
+				+ "	</a>" + "</div>" + "</body>" + "</html>";// 邮件内容
 		String subject = "重置密码通知";// 邮件主题
 		SimpleMailSender.sendHtmlMail(toMail, subject, content);
-		ResponseData<User> responseBody = new ResponseData<User>();
+		
 		responseBody.setResult(new Result(Status.OK, user));
 		return responseBody;
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/disableUser", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseData<User> disableUser(@RequestBody User user) {
-		
+
 		if (StringUtils.equals(user.getStatus(), UserConstant.NORMAL)) {
 			user.setStatus(UserConstant.DISABLE);
 		} else {
 			user.setStatus(UserConstant.NORMAL);
 		}
-		userService.updateByIdSelective(user);
 		ResponseData<User> responseBody = new ResponseData<User>();
-		responseBody.setResult(new Result(Status.OK, user));
+		try {
+
+			userService.updateByIdSelective(user);
+			responseBody.setResult(new Result(Status.OK, user));
+
+		} catch (PlatformException e) {
+			responseBody
+					.setResult(new Result(Status.ERROR, "disableUser faild"));
+
+			if (logger.isErrorEnabled()) {
+				logger.error("disableUser ", e);
+			}
+		}
+		
 		return responseBody;
 	}
-	
+
 	/**
 	 * 获取用户列表数据 重新组装关联数据
-	 * @param   
+	 * 
+	 * @param
 	 * @return
 	 */
 	@ResponseBody
 	@RequestMapping(value = "/queryUserList", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseData<User> queryUserList(HttpServletRequest request,@RequestBody User query ,PageRequest pageable ) {
-		
+	public ResponseData<User> queryUserList(HttpServletRequest request,
+			@RequestBody User query, PageRequest pageable) {
+
 		ResponseData<User> responseBody = new ResponseData<User>();
-		
-		Object obj = request.getSession().getAttribute("sessionUser"); 
-		if(obj == null){
-			responseBody.setResult(new Result(Status.ERROR, "validate loging session failed"));
+
+		Object obj = request.getSession().getAttribute("sessionUser");
+		if (obj == null) {
+			responseBody.setResult(
+					new Result(Status.ERROR, "validate loging session failed"));
 			return responseBody;
 		}
-		
+
 		try {
-			
+
 			Page<User> page = userService.queryUserList(query, pageable);
 			responseBody.setPageList(page);
 			responseBody.setResult(new Result(Status.OK, ""));
 			return responseBody;
-			
-		} catch (Exception e) {
-			responseBody.setResult(new Result(Status.ERROR, "queryUserList faild"));
-			
-			if(logger.isErrorEnabled()){
-				logger.error("queryUserList ",e);
+
+		} catch (PlatformException e) {
+			responseBody
+					.setResult(new Result(Status.ERROR, "queryUserList faild"));
+
+			if (logger.isErrorEnabled()) {
+				logger.error("queryUserList ", e);
 			}
 		}
-		
+
 		return responseBody;
 	}
 	@ResponseBody
@@ -146,13 +180,42 @@ public class UserController extends BaseControllerImpl<User, UserBo> {
 		try {
 			userService.updateUser(user);
 			result.setStatus(Status.OK);
-		} catch (PlatformException e){
+		} catch (PlatformException e) {
 			result.addError(e.getMessage());
 		} catch (Exception e) {
 			result.addError("系统错误");
-			logger.error("更新错误",e);
+			logger.error("更新错误", e);
 		}
 		responseBody.setResult(result);
 		return responseBody;
 	}
+
+	/**
+	 * 获取部门列表
+	 * @author zhaoying
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/departmentList", method = RequestMethod.GET)
+	public ResponseData<Department> departmentList() {
+
+		ResponseData<Department> responseBody = new ResponseData<Department>();
+		try {
+			// 部门列表
+			List<Department> deptList = departmentService.queryAll();
+			responseBody.setEntityList(deptList);
+			responseBody.setResult(new Result(Status.OK, ""));
+			return responseBody;
+
+		} catch (PlatformException e) {
+			responseBody
+					.setResult(new Result(Status.ERROR, "departmentList faild"));
+
+			if (logger.isErrorEnabled()) {
+				logger.error("departmentList ", e);
+			}
+		}
+		return responseBody;
+	}
+
 }
