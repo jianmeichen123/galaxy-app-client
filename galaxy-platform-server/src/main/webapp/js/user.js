db0c7bd30dd4f078952409c7f8d0b7fd59b1f3a2
zhangchunyuan <chunhuanzhang@galaxyinternet.com>
2016-03-03 15:56:56 +0800

    test

--- galaxy-platform-server/src/main/webapp/js/user.js
+++ galaxy-platform-server/src/main/webapp/js/user.js
@@ -1,12 +1,7 @@
 //用户相关的js
 var deptList ={"1":"1","2":'item2'};
 $(function() {
-	
-	
-	$("#popTxt").on("click","a[action='save']",function() {
-		alert("aaaa");
-	});
-	
+
 	var deptSelect =$('#selectDept'); 
 	
 	
@@ -18,21 +13,7 @@ $(function() {
 		deptSelect.append(option);
 	});
 	
-
-	
 });
-
-function save(){
-	var pop = $("#pop");
-	var json = {};
-	$(pop).find("input").each(function(){
-		if($(this).val().trim()!=""){
-			json[$(this).attr("name")]= $(this).val();
-		}
-	});
-	
-	console.log(json);
-}
 //检索
 function searchForm() {
 	var re = /^[0-9]+.?[0-9]*$/;
