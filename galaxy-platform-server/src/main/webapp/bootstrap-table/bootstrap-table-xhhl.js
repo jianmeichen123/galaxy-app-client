73ec154e7e7cf415c3828b5daf2802d87fdaa13c
zhangchunyuan <chunhuanzhang@galaxyinternet.com>
2016-03-07 14:03:59 +0800

    分页

--- galaxy-platform-server/src/main/webapp/bootstrap-table/bootstrap-table-xhhl.js
+++ galaxy-platform-server/src/main/webapp/bootstrap-table/bootstrap-table-xhhl.js
@@ -2383,7 +2383,6 @@
     
     ///
     BootstrapTable.prototype.querySearch = function (params) {
-    	console.log("params="+params);
         this.options.pageNumber = 1;
         this.initServer(params && params.silent, params && params.query);
     };
