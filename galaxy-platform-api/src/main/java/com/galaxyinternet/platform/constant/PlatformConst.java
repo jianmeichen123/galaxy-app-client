561772f4ac0f411b82ceee889a1c8264a75f3dd7
xingkaihu <kaihuxing@galaxyinternet.com>
2016-03-16 21:19:54 +0800

    重构代码

--- galaxy-platform-api/src/main/java/com/galaxyinternet/platform/constant/PlatformConst.java
+++ galaxy-platform-api/src/main/java/com/galaxyinternet/platform/constant/PlatformConst.java
@@ -5,6 +5,7 @@ public class PlatformConst {
 	public static final String REQUEST_SCOPE_MESSAGE_TIP = "galaxy_project_message_tip";
 	public static final String REQUEST_SCOPE_PROJECT_NAME = "galaxy_project_name";
 	public static final String REQUEST_SCOPE_PROJECT_ID = "galaxy_project_id";
+	public static final String REQUEST_SCOPE_URL_NUMBER = "galaxy_url_number";
 	public static final String PROJECT_TYPE = "项目";
 	public static final String TASK_TYPE = "任务";
 	public static final Integer MODULE_BROADCAST_MESSAGE = 1;//消息广播
