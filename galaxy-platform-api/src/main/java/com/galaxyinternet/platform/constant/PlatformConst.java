336c1203709cb2ebc07dc1e9c9cc348d02296fdd
vincent <leizhang@galaxyinternet.com>
2016-04-11 18:01:48 +0800

    上一次刷新时间

--- galaxy-platform-api/src/main/java/com/galaxyinternet/platform/constant/PlatformConst.java
+++ galaxy-platform-api/src/main/java/com/galaxyinternet/platform/constant/PlatformConst.java
@@ -9,4 +9,6 @@ public class PlatformConst {
 	public static final String PROJECT_TYPE = "项目";
 	public static final String TASK_TYPE = "任务";
 	public static final Integer MODULE_BROADCAST_MESSAGE = 1;//消息广播
+	
+	public static final String OPERATIO_NMESSAGE_TIME = "om:lasttime:";//上一次刷新时间
 }
