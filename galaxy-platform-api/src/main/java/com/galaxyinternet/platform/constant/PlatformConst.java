8a74572e4edd0c0d698bb834f1e91a58261e0cef
xingkaihu <kaihuxing@galaxyinternet.com>
2016-05-18 15:20:01 +0800

    添加常量定义

--- galaxy-platform-api/src/main/java/com/galaxyinternet/platform/constant/PlatformConst.java
+++ galaxy-platform-api/src/main/java/com/galaxyinternet/platform/constant/PlatformConst.java
@@ -13,4 +13,8 @@ public class PlatformConst {
 	public static final Integer MODULE_BROADCAST_MESSAGE = 1;//消息广播
 	
 	public static final String OPERATIO_NMESSAGE_TIME = "om:lasttime:";//上一次刷新时间
+	
+	public static final String REQUEST_SCOPE_IDEA_ID = "galaxy_idea_id";
+	public static final String REQUEST_SCOPE_IDEA_NAME = "galaxy_idea_name";
+	public static final String REQUEST_SCOPE_IDEA_CONTENT = "galaxy_idea_content";
 }
