34a39dd9580b284216c4d9de2d591aaca92436a7
zhongliangzhang <zhongliangzhang@galaxyinternet.com>
2016-05-31 11:29:21 +0800

    fkdjfkd

---
+++ galaxy-platform-server/src/main/java/com/galaxyinternet/resource/dao/impl/ResourceDaoImpl.java
@@ -0,0 +1,9 @@
+package com.galaxyinternet.resource.dao.impl;
+
+import com.galaxyinternet.framework.core.dao.impl.BaseDaoImpl;
+import com.galaxyinternet.model.resource.PlatformResource;
+import com.galaxyinternet.resource.dao.ResourceDao;
+
+public class ResourceDaoImpl extends BaseDaoImpl<PlatformResource, Long> implements ResourceDao {
+	
+}
---
+++ galaxy-platform-server/src/main/java/com/galaxyinternet/resource/service/impl/ResourceServiceImpl.java
@@ -0,0 +1,8 @@
+package com.galaxyinternet.resource.service.impl;
+
+import com.galaxyinternet.framework.core.dao.impl.BaseDaoImpl;
+import com.galaxyinternet.model.resource.PlatformResource;
+
+public class ResourceServiceImpl extends BaseDaoImpl<PlatformResource, Long> {
+
+}
