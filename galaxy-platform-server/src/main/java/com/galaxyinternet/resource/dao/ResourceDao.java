2df8e1d06d71cb82a2a3968103a4bbd4dd9a83b2
zhongliangzhang <zhongliangzhang@galaxyinternet.com>
2016-05-31 11:28:31 +0800

    资源 xml 做成

---
+++ galaxy-platform-server/src/main/java/com/galaxyinternet/resource/dao/ResourceDao.java
@@ -0,0 +1,8 @@
+package com.galaxyinternet.resource.dao;
+
+import com.galaxyinternet.framework.core.dao.BaseDao;
+import com.galaxyinternet.model.resource.PlatformResource;
+
+public interface ResourceDao extends BaseDao<PlatformResource, Long> {
+
+}
