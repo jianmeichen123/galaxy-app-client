bfbb0879c4ed91a1e467638489c6d9e10b759dc2
swang <songwang@galaxyinternet.com>
2016-07-26 16:16:16 +0800

    使用display:none

--- galaxy-platform-api/src/main/java/com/galaxyinternet/platform/tag/AclTag.java
+++ galaxy-platform-api/src/main/java/com/galaxyinternet/platform/tag/AclTag.java
@@ -1,9 +1,11 @@
 package com.galaxyinternet.platform.tag;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
 import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.TagSupport;
 
 import com.galaxyinternet.framework.core.constants.Constants;
@@ -28,20 +30,55 @@ public class AclTag extends TagSupport
 	@Override
 	public int doStartTag() throws JspException
 	{
-		HttpSession session = pageContext.getSession();
-		User user = (User)session.getAttribute(Constants.SESSION_USER_KEY);
-		List<PlatformResource> resources = user.getAllResourceToUser();
-		if(resources != null && resources.size() >0)
+		try
 		{
-			for(PlatformResource resource : resources)
+			
+			boolean hasResource = false;
+			HttpSession session = pageContext.getSession();
+			User user = (User)session.getAttribute(Constants.SESSION_USER_KEY);
+			List<PlatformResource> resources = user.getAllResourceToUser();
+			if(resources != null && resources.size() >0)
 			{
-				if(resourceMark != null && resourceMark.equals(resource.getResourceMark()))
+				for(PlatformResource resource : resources)
 				{
-					return EVAL_BODY_INCLUDE;
+					if(resourceMark != null && resourceMark.equals(resource.getResourceMark()))
+					{
+						hasResource = true;
+						break;
+					}
 				}
 			}
+			String start = "<div";
+			if(!hasResource)
+			{
+				start += " style=\"display:none;\"";
+			}
+			start +=">";
+			JspWriter out = pageContext.getOut();
+			out.write(start);
 		}
-		
-		return SKIP_BODY;
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		return EVAL_BODY_INCLUDE;
 	}
+
+	@Override
+	public int doEndTag() throws JspException
+	{
+		try
+		{
+			JspWriter out = pageContext.getOut();
+			out.write("</div>");
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return super.doEndTag();
+	}
+	
+	
 }
