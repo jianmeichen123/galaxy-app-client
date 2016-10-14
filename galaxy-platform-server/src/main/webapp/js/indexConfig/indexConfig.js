0cae6c9c63be070658de6f8059fba598dd81f105
zhangfeng <fengzhang@falaxyinternet.com>
2016-10-14 14:08:16 +0800

    js 首页配置

---
+++ galaxy-platform-server/src/main/webapp/js/indexConfig/indexConfig.js
@@ -0,0 +1,104 @@
+
+//var roleOrUser = $("#target_mark").val(); //为角色 或 用户
+//var targetVal = $("#target_val").val();  //角色 或 用户 id
+var roleOrUser = null;
+var targetVal = 0;
+
+/**
+ * 获取可配置项，赋值到radio 
+ */
+function queryAvailableConfig(){
+	sendGetRequest(platformUrl.queryAvailableConfig+"null/0",null,setRadioResource);
+}
+
+//设置radio选项
+function setRadioResource(data){
+	var result = data.result.status;
+	if(result == "ERROR"){ //OK, ERROR
+		layer.msg(data.result.message);
+		return;
+	}
+	var entityList = data.entityList;
+	if(entityList.length != 0 ){
+		for(var i=0;i<data.entityList.length;i++){
+	    	$("#resourceIdS").append("<input type='radio' name='resource' value='"+data.entityList[i].resourceId+"' />"+data.entityList[i].resourceName);
+	    } 
+	}
+}
+
+
+
+/**
+ * 获取已配置项，展示到页面 
+ */
+function queryModelConfig(){
+	sendGetRequest(platformUrl.queryModelConfig+"null/0",null,setModelConfig);
+}
+
+//展示到页面
+function setModelConfig(data){
+	var result = data.result.status;
+	if(result == "ERROR"){ //OK, ERROR
+		layer.msg(data.result.message);
+		return;
+	}
+	var entityList = data.entityList;
+	if(entityList.length != 0 ){
+		for(var i=0;i<data.entityList.length;i++){
+	    	
+	    } 
+	}
+}
+
+
+
+
+/**
+ * 页面 + 操作
+ * 可配置项 --> 页面配置 
+ */
+function configChange_1(){
+
+	
+}
+/**
+ * 页面 - 操作
+ * 页面配置  --> 可配置项 
+ */
+function configChange_2(){
+
+	
+}
+
+
+/**
+ * 获取页面配置模版 的值
+ */
+function model_result(){
+
+	
+}
+
+
+/**
+ * 保存配置模版的 数组 值
+ */
+function save_result(){
+	var params = model_result();
+	sendPostRequestByJsonObj(platformUrl.saveIndexModel+"null/0",params,saveCallBack);
+}
+
+//保存回调
+function saveCallBack(data){
+	var result = data.result.status;
+	
+	if(result == "ERROR"){ //OK, ERROR
+		layer.msg(data.result.message);
+		return;
+	}
+	layer.msg("保存成功", {time : 500 });
+}
+
+
+
+
