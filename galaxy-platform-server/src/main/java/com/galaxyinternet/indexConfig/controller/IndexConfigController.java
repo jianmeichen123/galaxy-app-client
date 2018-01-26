f8803ef90e357fecba1685ef071b1ac0c39fe6d6
zhangfeng <fengzhang@falaxyinternet.com>
2018-01-26 16:34:50 +0800

    danao data

--- galaxy-platform-server/src/main/java/com/galaxyinternet/indexConfig/controller/IndexConfigController.java
+++ galaxy-platform-server/src/main/java/com/galaxyinternet/indexConfig/controller/IndexConfigController.java
@@ -30,53 +30,53 @@ import java.util.Map;
 @Controller
 @RequestMapping("/galaxy/indexConfig")
 public class IndexConfigController extends BaseControllerImpl<IndexConfig, IndexConfigBo> {
-	
+
 	final Logger logger = LoggerFactory.getLogger(IndexConfigController.class);
-	
+
 	@Autowired
 	private IndexConfigService indexConfigService;
-	
+
 	@Autowired
 	com.galaxyinternet.framework.cache.Cache cache;
-	
+
 	@Autowired
 	AuthRequest authReq;
-	
+
 	@Override
 	protected BaseService<IndexConfig> getBaseService() {
 		return this.indexConfigService;
 	}
-	
-	
+
+
 	/**
-	 * 页面 
+	 * 页面
 	 */
 	@RequestMapping(value = "/toIndexConfig",  produces = MediaType.APPLICATION_JSON_VALUE)
 	public String toIndexConfig(HttpServletRequest request) {
 		return "system/desksetting/indexConfig";
 	}
-	
+
 	/**
-	 * 页面 
+	 * 页面
 	 */
 	@RequestMapping(value = "/toAddCon",  produces = MediaType.APPLICATION_JSON_VALUE)
 	public String toAddCon(HttpServletRequest request) {
 		return "system/desksetting/addCon";
 	}
-	
-	
+
+
 	/**
 	 * 管理员 获取 可配置项
 	 * resources 中 indexDivConfig = 1 的列表
 	 * 选择度 indexConfig 中 未配置的
-	 * 
+	 *
 	 * @param roleOrUser 标识 是为 某角色 或 某用户 配置首页
 	 * @param id 标识属性的id
 	 */
 	@ResponseBody
 	@RequestMapping(value = "/queryAvailableConfig/{roleOrUser}/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseData<IndexConfigBo> queryAvailableConfig(HttpServletRequest request,
-			@PathVariable("roleOrUser") String roleOrUser, @PathVariable("id") Long id) {
+															@PathVariable("roleOrUser") String roleOrUser, @PathVariable("id") Long id) {
 		ResponseData<IndexConfigBo> responseBody = new ResponseData<IndexConfigBo>();
 		try {
 			List<IndexConfigBo> configList = new ArrayList<>();
@@ -117,12 +117,12 @@ public class IndexConfigController extends BaseControllerImpl<IndexConfig, Index
 		}
 		return responseBody;
 	}
-	
 
-	
+
+
 	/**
 	 * 管理员 拉取  已配置项（获取全部， 所以不过滤null）
-	 * 
+	 *
 	 * @param roleOrUser 标识 是为 某角色 或 某用户 配置首页
 	 * @param id 标识属性的id
 	 */
@@ -135,7 +135,7 @@ public class IndexConfigController extends BaseControllerImpl<IndexConfig, Index
 			List<IndexConfigBo> configList = indexConfigService.queryConfigResource(params);
 			if(configList != null && configList.size() > 0)
 			{
-				
+
 				PlatformResource resQuery = new PlatformResource();
 				resQuery.setResourceType("5");//桌面组件
 				List<PlatformResource> resList = authReq.getResources(resQuery);
@@ -147,13 +147,13 @@ public class IndexConfigController extends BaseControllerImpl<IndexConfig, Index
 						codeNameMap.put(item.getResourceMark(), item.getResourceName());
 					}
 				}
-				
+
 				for(IndexConfigBo bo : configList)
 				{
 					bo.setResourceName(codeNameMap.get(bo.getResourceCode()));
 				}
 			}
-			
+
 			responseBody.setEntityList(configList);
 			responseBody.setResult(new Result(Status.OK, ""));
 		} catch (Exception e) {
@@ -162,7 +162,7 @@ public class IndexConfigController extends BaseControllerImpl<IndexConfig, Index
 		}
 		return responseBody;
 	}
-	
+
 	/**
 	 * 保存模型
 	 * @param roleOrUser 标识 是为 某角色 或 某用户 配置首页
@@ -171,7 +171,7 @@ public class IndexConfigController extends BaseControllerImpl<IndexConfig, Index
 	@ResponseBody
 	@RequestMapping(value = "/saveModel", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseData<IndexConfig> saveModel(HttpServletRequest request,
-			@RequestBody IndexConfig  indexConfig ) {
+											   @RequestBody IndexConfig  indexConfig ) {
 		ResponseData<IndexConfig> responseBody = new ResponseData<IndexConfig>();
 		try {
 			//根据资源id查询资源的详细信
@@ -197,88 +197,86 @@ public class IndexConfigController extends BaseControllerImpl<IndexConfig, Index
 			Long insert2 = indexConfigService.insert(de);
 			list.add(insert1);
 			list.add(insert2);
-			 Map<String,Object> map=new HashMap<String,Object>();
-			 map.put("arr", list);
-			 responseBody.setUserData(map);
-			 responseBody.setResult(new Result(Status.OK, "保存成功"));
+			Map<String,Object> map=new HashMap<String,Object>();
+			map.put("arr", list);
+			responseBody.setUserData(map);
+			responseBody.setResult(new Result(Status.OK, "保存成功"));
 		} catch (Exception e) {
 			responseBody.setResult(new Result(Status.ERROR, null,"保存失败"));
 			logger.error("首页配置保存 失败",e);
 		}
 		return responseBody;
 	}
-	
+
 	/**
 	 * 管理员 配置 某角色 或 某用户 首页后保存
-	 * 
+	 *
 	 * 删除  IndexConfig 中 某角色 或 某用户 的列表
 	 * 获取前台配置数据
 	 * 保存到 IndexConfig
-	 * 
+	 *
 	 * @param roleOrUser 标识 是为 某角色 或 某用户 配置首页
 	 * @param id 标识属性的id
 	 */
 	@ResponseBody
 	@RequestMapping(value = "/saveIndexConfig", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseData<IndexConfig> saveIndexConfig(HttpServletRequest request,
-			@RequestBody IndexConfig  indexConfig ) {
+													 @RequestBody IndexConfig  indexConfig ) {
 		ResponseData<IndexConfig> responseBody = new ResponseData<IndexConfig>();
 		try {
-			indexConfigService.insert(indexConfig);
 			//根据资源id查询资源的详细信息
-			 /*int insert = indexConfigService.updateById(indexConfig);
-			 if(insert>0){
-				 responseBody.setResult(new Result(Status.OK, "保存成功"));
-			 }else{
-				 responseBody.setResult(new Result(Status.ERROR, "保存失败")); 
-			 }*/
-			 
+			int insert = indexConfigService.updateById(indexConfig);
+			if(insert>0){
+				responseBody.setResult(new Result(Status.OK, "保存成功"));
+			}else{
+				responseBody.setResult(new Result(Status.ERROR, "保存失败"));
+			}
+
 		} catch (Exception e) {
 			responseBody.setResult(new Result(Status.ERROR, null,"保存失败"));
 			logger.error("首页配置保存 失败",e);
 		}
 		return responseBody;
 	}
-	
+
 	/**
 	 * 管理员 配置 某角色 或 某用户 首页后保存
-	 * 
+	 *
 	 * 删除  IndexConfig 中 某角色 或 某用户 的列表
 	 * 获取前台配置数据
 	 * 保存到 IndexConfig
-	 * 
+	 *
 	 * @param roleOrUser 标识 是为 某角色 或 某用户 配置首页
 	 * @param id 标识属性的id
 	 */
 	@ResponseBody
 	@RequestMapping(value = "/deleteIndexModel", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseData<IndexConfig> deleteIndexModel(HttpServletRequest request,
-			@RequestBody IndexConfig  indexConfig ) {
+													  @RequestBody IndexConfig  indexConfig ) {
 		ResponseData<IndexConfig> responseBody = new ResponseData<IndexConfig>();
 		try {
 			//根据资源id查询资源的详细信息
 			//indexConfig.setResourceId(null);
-			indexConfigService.deleteById(indexConfig.getId());
-			//int updateByResourceId = indexConfigService.updateByResourceId(indexConfig);
-		//	 int delete = indexConfigService.delete(indexConfig);
-			/* if(updateByResourceId>0){
-				 responseBody.setResult(new Result(Status.OK, "删除配置项成功"));
-			 }else{
-				 responseBody.setResult(new Result(Status.ERROR, "删除配置项失败"));
-			 }*/
-			 
+			int updateByResourceId = indexConfigService.updateByResourceId(indexConfig);
+			//	 int delete = indexConfigService.delete(indexConfig);
+			if(updateByResourceId>0){
+				responseBody.setResult(new Result(Status.OK, "删除配置项成功"));
+			}else{
+				responseBody.setResult(new Result(Status.ERROR, "删除配置项失败"));
+			}
+
 		} catch (Exception e) {
 			responseBody.setResult(new Result(Status.ERROR, null,"保存失败"));
 			logger.error("首页配置保存 失败",e);
 		}
 		return responseBody;
 	}
-	
-	
-	
-	
-	
 
 
-	
+
+
+
+
+
+
 }
--- galaxy-platform-server/src/main/webapp/js/indexConfig/indexConfig.js
+++ galaxy-platform-server/src/main/webapp/js/indexConfig/indexConfig.js
@@ -5,49 +5,49 @@ var roleOrUser = null;
 var targetVal = 0;
 
 /**
- * 获取可配置项，赋值到radio 
+ * 获取可配置项，赋值到radio
  */
 function queryAvailableConfig(){
-	sendGetRequest(platformUrl.queryAvailableConfig+"null/0",null,setRadioResource);
+    sendGetRequest(platformUrl.queryAvailableConfig+"null/0",null,setRadioResource);
 }
 
 //设置radio选项
 function setRadioResource(data){
-	var result = data.result.status;
-	if(result == "ERROR"){ //OK, ERROR
-		layer.msg(data.result.message);
-		return;
-	}
-	var entityList = data.entityList;
-	if(entityList.length != 0 ){
-		for(var i=0;i<data.entityList.length;i++){
-	    	$("#resourceIdS").append("<label><input type='radio' name='resource' value='"+data.entityList[i].resourceId+"' data-code='"+data.entityList[i].resourceMark+"' data-url='"+data.entityList[i].contentUrl+"'/>"+data.entityList[i].resourceName)+"</label>";
-	    } 
-	}
+    var result = data.result.status;
+    if(result == "ERROR"){ //OK, ERROR
+        layer.msg(data.result.message);
+        return;
+    }
+    var entityList = data.entityList;
+    if(entityList.length != 0 ){
+        for(var i=0;i<data.entityList.length;i++){
+            $("#resourceIdS").append("<label><input type='radio' name='resource' value='"+data.entityList[i].resourceId+"' data-code='"+data.entityList[i].resourceMark+"' data-url='"+data.entityList[i].contentUrl+"'/>"+data.entityList[i].resourceName)+"</label>";
+        }
+    }
 }
 
 
 
 /**
- * 获取已配置项，展示到页面 
+ * 获取已配置项，展示到页面
  */
 function queryModelConfig(){
-	sendGetRequest(platformUrl.queryModelConfig+"null/0",null,setModelConfig);
+    sendGetRequest(platformUrl.queryModelConfig+"null/0",null,setModelConfig);
 }
 
 //展示到页面
 function setModelConfig(data){
-	var result = data.result.status;
-	if(result == "ERROR"){ //OK, ERROR
-		layer.msg(data.result.message);
-		return;
-	}
-	var entityList = data.entityList;
-	if(entityList.length != 0 ){
-		for(var i=0;i<data.entityList.length;i++){
-	    	
-	    } 
-	}
+    var result = data.result.status;
+    if(result == "ERROR"){ //OK, ERROR
+        layer.msg(data.result.message);
+        return;
+    }
+    var entityList = data.entityList;
+    if(entityList.length != 0 ){
+        for(var i=0;i<data.entityList.length;i++){
+
+        }
+    }
 }
 
 
@@ -55,19 +55,19 @@ function setModelConfig(data){
 
 /**
  * 页面 + 操作
- * 可配置项 --> 页面配置 
+ * 可配置项 --> 页面配置
  */
 function configChange_1(){
 
-	
+
 }
 /**
  * 页面 - 操作
- * 页面配置  --> 可配置项 
+ * 页面配置  --> 可配置项
  */
 function configChange_2(){
 
-	
+
 }
 
 
@@ -75,17 +75,17 @@ function configChange_2(){
  * 获取页面配置模版 的值
  */
 function model_result(id,configOrder){
-	var item = $('#resourceIdS input[name="resource"]:checked ');
-	var valu=item.val();
-	var code = item.data('code')
-	var url = item.data('url');
-	var data={
-			"id":id,
-			"resourceId":valu,
-			"resourceCode": code,
-			"contentUrl":url
-	}
-	return data;
+    var item = $('#resourceIdS input[name="resource"]:checked ');
+    var valu=item.val();
+    var code = item.data('code')
+    var url = item.data('url');
+    var data={
+        "id":id,
+        "resourceId":valu,
+        "resourceCode": code,
+        "contentUrl":url
+    }
+    return data;
 }
 
 
@@ -93,51 +93,51 @@ function model_result(id,configOrder){
  * 保存配置模版的 数组 值
  */
 function save_result(id,configOrder){
-	var params = model_result(id,configOrder);
-	sendPostRequestByJsonObj(platformUrl.saveIndexModel,params,function(data){
-		var result = data.result.status;
-		if(result == "ERROR"){ //OK, ERROR
-			layer.msg(data.result.message);
-			return;
-		}
-		layer.msg("保存成功");
-	});
-	return true;
+    var params = model_result(id,configOrder);
+    sendPostRequestByJsonObj(platformUrl.saveIndexModel,params,function(data){
+        var result = data.result.status;
+        if(result == "ERROR"){ //OK, ERROR
+            layer.msg(data.result.message);
+            return;
+        }
+        layer.msg("保存成功");
+    });
+    return true;
 }
 
 function deleteIndexConfig(resourceId){
-	var datas={
-		"id":resourceId
-	};
-	sendPostRequestByJsonObj(platformUrl.deleteIndexModel,datas,function(data){
-		var result = data.result.status;
-		if(result == "ERROR"){ //OK, ERROR
-			layer.msg(data.result.message);
-			return;
-		}
-		layer.msg("删除配置成功");	
-		
-	});
-	return true;
+    var datas={
+        "resourceId":resourceId
+    };
+    sendPostRequestByJsonObj(platformUrl.deleteIndexModel,datas,function(data){
+        var result = data.result.status;
+        if(result == "ERROR"){ //OK, ERROR
+            layer.msg(data.result.message);
+            return;
+        }
+        layer.msg("删除配置成功");
+
+    });
+    return true;
 }
 
 function addBlock(shapeType,configOrder){
-	var datas={
-		"shapeType":shapeType,
-		"configOrder":configOrder
-	};
-	var reslut2;
-	sendPostRequestByJsonObj(platformUrl.saveModel,datas,function(data){
-		var result = data.result.status;
-		if(result == "ERROR"){ //OK, ERROR
-			layer.msg(data.result.message);
-			return;
-		}
-	//	layer.msg("删除配置成功");	
-	reslut2=data.userData.arr;
-	});
-	
-	return reslut2;
-	
+    var datas={
+        "shapeType":shapeType,
+        "configOrder":configOrder
+    };
+    var reslut2;
+    sendPostRequestByJsonObj(platformUrl.saveModel,datas,function(data){
+        var result = data.result.status;
+        if(result == "ERROR"){ //OK, ERROR
+            layer.msg(data.result.message);
+            return;
+        }
+        //	layer.msg("删除配置成功");
+        reslut2=data.userData.arr;
+    });
+
+    return reslut2;
+
 }
 
